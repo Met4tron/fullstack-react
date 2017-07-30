2dd142acf658284114798af1e91064bf1d8b93b7
Metatron <yuurig@gmail.com>
2017-07-30 12:22:48 -0300

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({
+        hello: 'world!!'
+    });
 });
 
 app.listen(PORT);
