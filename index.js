601ec9212a6f088c5d28b11f4a0cc00e2d43a00d
Metatron <yuurig@gmail.com>
2017-08-05 13:45:07 -0300

    mudanças feitas na api

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 const app = express();
+
+require('./models/User');
+require('./services/passport');
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+mongoose.connect(keys.mongoURI, {useMongoClient: true});
+mongoose.Promise = global.Promise;
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send({
-        hello: 'world!!'
-    });
-});
-
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Connected on PORT ${PORT}`));
